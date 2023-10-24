de5245de296b838c79c623e5d391276b6d3181ba
alidaaar <145494938+alidaaar@users.noreply.github.com>
2023-10-24 16:50:18 +0600

    Add files via upload

---
+++ Лаб1 без опыта/Задача 1 без опыта.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+    double a, b;
+
+    cin >> a >> b;
+
+    double c = sqrt(a * a + b * b);
+
+    cout << c << endl;
+
+    return 0;
+}
